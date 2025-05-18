752e52c91dc7fdcdefa06ad7c2fa00e444db2d2a
Jo <jonathanholly@bol.com.br>
2025-05-18 01:00:08 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,66 @@
+﻿
+using MauiAppHotel.Models;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+
+    {
+
+        public List<Quarto> lista_quartos = new List<Quarto>
+
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte super luxo",
+                ValorDiariaAdulto = 110.00,
+                ValorDiariaCrianca = 55.00
+
+            },
+
+             new Quarto()
+            {
+                Descricao = "Suíte luxo",
+                ValorDiariaAdulto = 80.00,
+                ValorDiariaCrianca = 40.00
+
+            },
+              new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50.00,
+                ValorDiariaCrianca = 25.00
+
+            },
+               new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariaAdulto = 25.00,
+                ValorDiariaCrianca = 12.50
+
+            }
+
+
+
+        };
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.ContratacaodaHospedagem());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+
+
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiAppHotel
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Kalam-Bold.ttf", "KalamBold");
+                    fonts.AddFont("Kalam-Light.ttf", "KalamLight");
+                    fonts.AddFont("Kalam-Regular.ttf", "Kalam");
+                    fonts.AddFont("Lato-Regular.ttf", "Lato");
+                    fonts.AddFont("Silkscreen-Regular.ttf", "Silkscreen");
+                    fonts.AddFont("PinyonScript-Regular.ttf", "PinyonScript");
+                    fonts.AddFont("DeliusUnicase-Regular.ttf", "DeliusUnicase");
+                    fonts.AddFont("Exo2-Regular.ttf", "Exo2");
+
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
